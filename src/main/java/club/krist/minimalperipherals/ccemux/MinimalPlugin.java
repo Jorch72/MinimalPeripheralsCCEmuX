9421fb77bdc9950395ea59e7f2859e2369cf44e5
Drew Lemmy <drew@lemmmy.pw>
2017-05-07 19:45:12 +0100

    configurable path

--- src/main/java/club/krist/minimalperipherals/ccemux/MinimalPlugin.java
+++ src/main/java/club/krist/minimalperipherals/ccemux/MinimalPlugin.java
@@ -3,11 +3,22 @@ package club.krist.minimalperipherals.ccemux;
 import club.krist.minimalperipherals.ccemux.sound.SoundSystem;
 import net.clgd.ccemux.peripherals.PeripheralFactory;
 import net.clgd.ccemux.plugins.Plugin;
+import net.clgd.ccemux.plugins.config.JSONConfigHandler;
+import net.clgd.ccemux.plugins.config.PluginConfigHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class MinimalPlugin extends Plugin {
+    private static final Logger log = LoggerFactory.getLogger(MinimalPlugin.class);
+    
+    private Path minecraftDirectory;
+    
     @Override
     public String getName() {
         return "MinimalPeripheralsCCEmuX";
@@ -32,16 +43,32 @@ public class MinimalPlugin extends Plugin {
     public Optional<String> getWebsite() {
         return Optional.empty();
     }
-
+    
+    @Override
+    public Optional<PluginConfigHandler<?>> getConfigHandler() {
+        return Optional.of(new JSONConfigHandler<Map<String, String>>(new HashMap<>()) {
+            @Override
+            public void configLoaded(Map<String, String> config) {
+                if (config.containsKey("minecraftDir")) {
+                    minecraftDirectory = Paths.get(config.get("minecraftDir"));
+                }
+            }
+        });
+    }
+    
     @Override
     public void setup() {
         PeripheralFactory.implementations.put("iron_noteblock", IronNoteblock::new);
         
-        Path minecraftDirectory = OperatingSystem.get().getAppDataDir().resolve(".minecraft");
+        if (minecraftDirectory == null) {
+            minecraftDirectory = OperatingSystem.get().getAppDataDir().resolve(".minecraft");
+        }
         
         if (!minecraftDirectory.toFile().exists()) {
-            throw new RuntimeException("Minecraft directory not found."); // TODO: config option
+            throw new RuntimeException("Minecraft directory not found.");
         }
+        
+        log.info("Loading minecraft assets from {}", minecraftDirectory.toString());
         
         new SoundSystem(minecraftDirectory.resolve("assets").toFile(), "1.11");
     }
